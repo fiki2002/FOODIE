97cde3d90a3abd6c4da40b608934f282bca4892d
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-08-10 23:50:19 +0100

    Pushed

--- lib/Screens/promo.dart
+++ lib/Screens/promo.dart
@@ -13,64 +13,80 @@ class _PromoPageState extends State<PromoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffF8F5F2),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 23,
-          right: 15,
-          top: 68,
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+             
+              Colors.white,
+               Color(0xffF8F5F2),
+            ],
+            stops: [0.0, 0.1],
+          ),
         ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SvgPicture.asset('assets/images/back.svg'),
-                Text(
-                  'Today\'s Promo',
-                  style: GoogleFonts.poppins(
-                    color: const Color(0xff3E4462),
-                    fontWeight: FontWeight.w500,
-                    fontSize: 24,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 23,
+            right: 15,
+            top: 68,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset('assets/images/back.svg'),
+                  Text(
+                    'Today\'s Promo',
+                    style: GoogleFonts.poppins(
+                      color: const Color(0xff3E4462),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 10,
+                  const SizedBox(
+                    width: 20,
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SvgPicture.asset('assets/images/filters.svg'),
-                      Text(
-                        'Filters',
-                        style: GoogleFonts.poppins(
-                          color: const Color(0xff3E4462),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/images/filters.svg'),
+                        const SizedBox(
+                          width: 8,
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ],
+                        Text(
+                          'Filters',
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xff3E4462),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );

2354eef14020b6f8b4931a6a7d6f427e61991257
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-08-11 23:21:33 +0100

    Another day to push this

--- lib/Screens/promo.dart
+++ lib/Screens/promo.dart
@@ -20,18 +20,17 @@ class _PromoPageState extends State<PromoPage> {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-             
               Colors.white,
-               Color(0xffF8F5F2),
+              Color(0xffF8F5F2),
             ],
-            stops: [0.0, 0.1],
+            stops: [0.05, 0.5],
           ),
         ),
         child: Padding(
           padding: const EdgeInsets.only(
             left: 23,
             right: 15,
-            top: 68,
+            top: 50,
           ),
           child: Column(
             children: [
@@ -57,37 +56,46 @@ class _PromoPageState extends State<PromoPage> {
               ),
               Row(
                 children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Row(
-                      children: [
-                        SvgPicture.asset('assets/images/filters.svg'),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          'Filters',
-                          style: GoogleFonts.poppins(
-                            color: const Color(0xff3E4462),
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
+                 
                 ],
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildContainer(
+     String title,
+     Color textColor,
+     String url,
+     bool isSelected ,
+  ) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: isSelected ? const Color(0xffE84C4F) : Colors.white,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Row(
+        children: [
+          SvgPicture.asset(url),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            title,
+            style: GoogleFonts.poppins(
+              color: const Color(0xff3E4462),
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
       ),
     );
   }

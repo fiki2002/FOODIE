2e91418a4fec1920a0d426a533db436e5fd88491
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-08-23 08:59:38 -0400

    Added global app theme

---
+++ lib/cores/constants/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThemeClass {
+  static const Color primaryColor = Color(0xff3E4462);
+  static const Color secondaryColor = Color(0xff7E7E7E);
+  static const Color brandPrimary = Color(0xffE84C4F);
+  static const Color brandSecondary = Color(0xffEDA345);
+  static const Color containerColor = Color(0xffFFFFFF);
+  static const Color containerMColor = Color(0xffE1E1E1);
+  
+}

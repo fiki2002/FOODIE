cf65e42a35df2bd2b2c4c2f7f374c57739a8e495
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-08-28 21:52:53 +0100

    Pushed

--- lib/features/screens/checkout_screen.dart
+++ lib/features/screens/checkout_screen.dart
@@ -4,29 +4,36 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../cores/constants/color.dart';
 
-class CheckOutScreen extends StatelessWidget {
+class CheckOutScreen extends StatefulWidget {
   const CheckOutScreen({Key? key}) : super(key: key);
 
+  @override
+  State<CheckOutScreen> createState() => _CheckOutScreenState();
+}
+
+class _CheckOutScreenState extends State<CheckOutScreen> {
+  bool _value = false;
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: const Color(0xffF8F5F2),
-      body: SafeArea(
-        child: Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffF8F5F2),
+        body: Column(
           children: [
             Container(
               width: w,
-              height: h * 0.02,
+              height: h * 0.12,
               padding: const EdgeInsets.symmetric(
-                vertical: 20,
+                vertical: 10,
                 horizontal: 15,
               ),
               decoration: const BoxDecoration(
                 color: ThemeClass.containerColor,
               ),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   GestureDetector(
                     onTap: () {
@@ -34,16 +41,79 @@ class CheckOutScreen extends StatelessWidget {
                     },
                     child: SvgPicture.asset(
                       'assets/images/back.svg',
-                      width: 10,
-                      height: 10,
+                      width: 20,
+                      height: 20,
                     ),
                   ),
                   Text(
                     'Checkout',
                     style: GoogleFonts.poppins(
-                      color: ThemeClass.containerColor,
+                      color: ThemeClass.primaryColor,
                       fontSize: 24,
                       fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 9,
+                vertical: 10,
+              ),
+              decoration: BoxDecoration(
+                color: ThemeClass.containerColor,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Keep social distancing',
+                        style: GoogleFonts.poppins(
+                          color: ThemeClass.primaryColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        'Leave your order on the doorstep',
+                        style: GoogleFonts.poppins(
+                          color: ThemeClass.secondaryColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Expanded(
+                    child: SizedBox(),
+                  ),
+                  Transform.scale(
+                    scale: 1,
+                    child: Switch(
+                      value: _value,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _value = value;
+                          },
+                        );
+                      },
+                      activeColor: ThemeClass.brandPrimary,
+                      activeTrackColor: const Color(0xffEFEFF2),
                     ),
                   ),
                 ],
